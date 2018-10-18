899eeb5600b293e725cfcba80d6298ee25f3d008
SeaCrap <506076395@qq.com>
2018-10-18 23:47:04 +0800

    JS 切换状态

--- main.js
+++ main.js
@@ -1,52 +1,34 @@
-// 这样有个bug，3跟1一起动画的时候，由于我们监听过1每次动画完都会添加一个right类，处理bug的方法很简单，改成临时监听，.on改成，one
-setTimeout(function(){
-    // 1-2一起做动画
-    $('.images > img:nth-child(1)').css({
-    transform: 'translateX(-100%)'
-    })
-    $('.images > img:nth-child(2)').css({
-    transform: 'translateX(-100%)'
-    })
-    // 1播放完回到右边
-    $('.images > img:nth-child(1)').one('transitionend',function(e){
-        $(e.currentTarget).addClass('rigth').css({
-            transform: 'none'
+$('.images > img:nth-child(1)').addClass('current')
+$('.images > img:nth-child(2)').addClass('enter')
+$('.images > img:nth-child(3)').addClass('enter')
+setTimeout(() => {
+    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
+        .one('transitionend',(e) => {
+            $(e.currentTarget).removeClass('leave').addClass('enter')
         })
-    })
-
+    $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
 },2000)
 
-
-setTimeout(function(){
-    // 2-3一起做动画
-    $('.images > img:nth-child(2)').css({
-    transform: 'translateX(-200%)'
-    })
-    $('.images > img:nth-child(3)').css({
-    transform: 'translateX(-100%)'
-    })
-    // 2播放完回到右边
-    $('.images > img:nth-child(2)').one('transitionend',function(e){
-        $(e.currentTarget).addClass('rigth').css({
-            transform: 'none'
+setTimeout(() => {
+    $('.images > img:nth-child(2)').removeClass('current').addClass('leave')
+        .one('transitionend',(e) => {
+            $(e.currentTarget).removeClass('leave').addClass('enter')
         })
-    })
-
+    $('.images > img:nth-child(3)').removeClass('enter').addClass('current')
 },4000)
 
-setTimeout(function(){
-    // 3-1一起做动画
-    $('.images > img:nth-child(3)').css({
-    transform: 'translateX(-200%)'
-    })
-    $('.images > img:nth-child(1)').css({
-    transform: 'translateX(-100%)'
-    })
-    // 3播放完回到右边
-    $('.images > img:nth-child(3)').one('transitionend',function(e){
-        $(e.currentTarget).addClass('rigth').css({
-            transform: 'none'
+setTimeout(() => {
+    $('.images > img:nth-child(3)').removeClass('current').addClass('leave')
+        .one('transitionend',(e) => {
+            $(e.currentTarget).removeClass('leave').addClass('enter')
         })
-    })
-
+    $('.images > img:nth-child(1)').removeClass('enter').addClass('current')
 },6000)
+
+//setTimeout(() => {
+//    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
+//        .one('transitionend',(e) => {
+//            $(e.currentTarget).removeClass('leave').addClass('enter')
+//        })
+//    $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
+//},9000)
