dd1d0d111667de400c12f911e13f9a5d61bd87fe
SeaCrap <506076395@qq.com>
2018-10-19 22:55:24 +0800

    优化后

--- main.js
+++ main.js
@@ -1,29 +1,67 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-
-let n = 1
-setInterval(() => {// 这里 setTimeout 改成setInterval
-   console.log(n)
-    //$('.images > img:nth-child(n)'),这里是从第一张开始 没有第n个
-    //$('.images > img:nth-child(n)') 改成ES6语法 $(`.images > img:nth-child(${n})`)
-    //${n}:意思是 n 是几 这里就是几
-    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
-        .one('transitionend',(e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
+初始化()
+setInterval(() => {
+   makeLeave(getImage(n))//这里返回值是 undefined 后边接 .one 会报错
+        .one('transitionend',(e) => {//transitionend 上个动画结束后)
+          //e.currentTarget 当前元素
+          // let 当前元素 = e.currentTarget
+          //makeEnter($(当前元素))
+            makeEnter($(e.currentTarget)) 
         })
-    $(`.images > img:nth-child(${x(n + 1)})`).removeClass('enter').addClass('current')
+    makeCurrent(getImage(n + 1))
     n += 1
 },3000)
 
-//定义一个函数 确保 n + 1 也在 1~3 之中
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*********下边是封装的函数**********/
+
+
+//确保 n 范围一直在 1~3
 function x(n){
  if(n > 3){
-    n = n % 3 // n等于4时，n%3等于1,也就是n等于4时，让n等于1
+    n = n % 3 
     if(n === 0){
-      n = 3 // 当n等于6，n%3等于0，这时让n等于3
-    }// n 一直会等于 1 2 3 
+      n = 3 
+    }
   }
   return n 
 }
 
+//初始化
+function 初始化(){
+  n = 1
+  $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter') 
+}
+
+//current 状态
+function makeCurrent($node){
+  return $node.removeClass('enter leave').addClass('current')
+}
+//leave 状态
+function makeLeave($node){
+  return $node.removeClass('current enter').addClass('leave')
+}
+//enter 状态
+function makeEnter($node){
+  return $node.removeClass('current enter').addClass('enter')
+}
+
+//getImage
+function getImage(n){
+  return $(`.images > img:nth-child(${x(n)}`)
+}
