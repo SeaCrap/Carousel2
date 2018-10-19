5ca53e48ded7a09eaeebaa0f6f33b1a07ee0fb91
SeaCrap <506076395@qq.com>
2018-10-19 21:00:55 +0800

    setInterval 循环轮播(会出现n+1等于4的BUG)

--- main.js
+++ main.js
@@ -1,34 +1,24 @@
 $('.images > img:nth-child(1)').addClass('current')
 $('.images > img:nth-child(2)').addClass('enter')
 $('.images > img:nth-child(3)').addClass('enter')
-setTimeout(() => {
-    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
+
+let n = 1
+setInterval(() => {// 这里 setTimeout 改成setInterval
+  if(n > 3){
+    n = n % 3 // n等于4时，n%3等于1,也就是n等于4时，让n等于1
+    if(n === 0){
+      n = 3 // 当n等于6，n%3等于0，这时让n等于3
+    }// n 一直会等于 1 2 3 
+  }
+  console.log(n)
+    //$('.images > img:nth-child(n)'),这里是从第一张开始 没有第n个
+    //$('.images > img:nth-child(n)') 改成ES6语法 $(`.images > img:nth-child(${n})`)
+    //${n}:意思是 n 是几 这里就是几
+    $(`.images > img:nth-child(${n})`).removeClass('current').addClass('leave')
         .one('transitionend',(e) => {
             $(e.currentTarget).removeClass('leave').addClass('enter')
         })
-    $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
-},2000)
+    $(`.images > img:nth-child(${n + 1})`).removeClass('enter').addClass('current')
+    n += 1
+},n * 3000)
 
-setTimeout(() => {
-    $('.images > img:nth-child(2)').removeClass('current').addClass('leave')
-        .one('transitionend',(e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
-        })
-    $('.images > img:nth-child(3)').removeClass('enter').addClass('current')
-},4000)
-
-setTimeout(() => {
-    $('.images > img:nth-child(3)').removeClass('current').addClass('leave')
-        .one('transitionend',(e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
-        })
-    $('.images > img:nth-child(1)').removeClass('enter').addClass('current')
-},6000)
-
-//setTimeout(() => {
-//    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
-//        .one('transitionend',(e) => {
-//            $(e.currentTarget).removeClass('leave').addClass('enter')
-//        })
-//    $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
-//},9000)
